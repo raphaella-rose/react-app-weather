e9ee07aa3e26927fad6af385c91803edf4b85b1c
raphaella-rose <raphaella.rose1@gmail.com>
2021-08-10 11:51:08 +0100

    integrated api

--- src/Weather.js
+++ src/Weather.js
@@ -2,14 +2,13 @@ import React from "react";
 import Footer from "./components/Footer";
 import SearchEngine from "./components/SearchEngine";
 import ForecastWrapper from "./components/ForecastWrapper";
-
 import "./styles/Weather.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
       <div className="Weather-App">
-        <SearchEngine />
+        <SearchEngine defaultCity="London" />
         <ForecastWrapper />
       </div>
       <Footer />
--- src/components/SearchEngine.js
+++ src/components/SearchEngine.js
@@ -1,8 +1,28 @@
-import React from "react";
-import Sun from "../images/sun.png";
+import React, { useState } from "react";
+import axios from "axios";
 import "../styles/SearchEngine.css";
 
-export default function SearchEngine() {
+export default function SearchEngine(props) {
+const [weatherData, setWeatherData] = useState({ ready: false });
+function handleResponse(response) {
+console.log(response.data)
+  setWeatherData({   
+      ready: true,
+      Temperature: response.data.main.temp,
+      Date: "Monday 11:00",
+      Description: response.data.weather[0].description,
+      Humidity: response.data.main.humidity,
+      Wind: response.data.wind.speed,
+      High: response.data.main.temp_max,
+      Low: response.data.main.temp_min,
+      Name: response.data.name,
+      Icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`})
+
+
+  
+}
+
+if (weatherData.ready) {
   return (
     <div className="SearchEngine">
     <form>
@@ -14,22 +34,32 @@ export default function SearchEngine() {
       />
       <input className="go-button" type="submit" value="Go" />
     </form>
-      <h1>London</h1>
+      <h1>{weatherData.Name}</h1>
       <div className="row">
         <div className="col-6">
           <ul className="current-details">
-            <li className="todays-date">Monday 11:00</li>
-            <li className="weather-description">Sunny</li>
-            <li className="high-low-temp">High: 20° Low: 16°</li>
-            <li className="windspeed">Windspeed: 8 Km/H</li>
-            <li className="humidity">Humidity: 83%</li>
+            <li className="todays-date">{weatherData.Date}</li>
+            <li className="weather-description">{weatherData.Description}</li>
+            <li className="high-low-temp">High: {Math.round(weatherData.High)}° Low: {Math.round(weatherData.Low)}°</li>
+            <li className="windspeed">Windspeed: {Math.round(weatherData.Wind)} km/h</li>
+            <li className="humidity">Humidity: {weatherData.Humidity}%</li>
           </ul>
         </div>
         <div className="col-6 current-weather">
-          <img className="pic" alt="Sun" src={Sun} />
-          <strong className="current-temperature">16</strong>
+          <img className="pic" alt={weatherData.Description} src={weatherData.Icon} />
+          <strong className="current-temperature">{Math.round(weatherData.Temperature)}</strong>
           <small className="unit-selector celcius">°C</small>
         </div>
       </div>
     </div>);
+} else {
+  let apiKey = "e99a6b83a02b9bd00c5d8a973f6c63e0"; 
+ let units = "metric";
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&units=${units}&appid=${apiKey}`;
+ axios.get(apiUrl).then(handleResponse);
+  return "loading..."
 }
+}
+
+ 
+
